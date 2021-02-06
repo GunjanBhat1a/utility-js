248f8a151b2124490fe0736f20558edd19f055d8
GunjanBhat1a <gunjan.bhatia@thoughtworks.com>
2021-02-06 15:44:47 +0530

    ADD: utility function to get head

---
+++ src/head.js
@@ -0,0 +1,7 @@
+const head = (nums) => {
+    if(nums.length > 0)
+    return nums[0];
+    return undefined;
+};
+
+module.exports = head;
---
+++ src/head.spec.js
@@ -0,0 +1,11 @@
+const head = require("./head");
+
+describe("head", () => {
+    it(("should return head of a non-empty array"), () => {
+        expect(head([1, 2, 3, 4])).toBe(1);
+    });
+    
+    it(("should return null or undefined for an empty array"), () => {
+        expect(head([])).toBe(undefined);
+    });
+});
